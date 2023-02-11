ef0c4f630b2f2ac114e11a45a2d14cac7251ae1c
Sandra <114067731+sgolebiewska@users.noreply.github.com>
2023-02-11 01:48:36 +0100

    Add files via upload

---
+++ leetcode3.java
@@ -0,0 +1,15 @@
+//Given a string s, find the length of the longest substring without repeating characters.
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        final int n = s.length();
+        int len = 0;
+        int [] repeat = new int[128];
+        for (int c = 0, j = 0, i = 0; j < n; j++) {
+            c = s.charAt(j);
+            i = Math.max(repeat[c], i);
+            len = Math.max(len, j - i +1);
+            repeat[c] = j+1;
+        }
+        return len;
+    }
+}
